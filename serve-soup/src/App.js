436850b78550532945421a4fe2fa03b0ae9a41ed
fritzgt <tuanis@live.com>
2019-07-11 22:42:42 -0400

    update the file structure

--- serve-soup/src/App.js
+++ serve-soup/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//importing homepage component which includes
+//a list of items
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
